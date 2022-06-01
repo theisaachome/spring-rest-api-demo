5e8e2dc6443efc29ab404b4623a3c15fb499c20b
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-02 00:17:14 +0630

    start project

---
+++ spring-blog-api/src/test/java/com/isaachome/blog/SpringBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaachome.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
